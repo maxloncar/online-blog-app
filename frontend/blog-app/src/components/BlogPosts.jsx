2f4d186b157d76153b2a9cd25fa55f3faa6e17ca
Max <loncarmax@gmail.com>
2022-09-10 23:38:23 +0200

    Implement search through blogs

--- frontend/blog-app/src/components/BlogPosts.jsx
+++ frontend/blog-app/src/components/BlogPosts.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import BlogPost from "./BlogPost";
 
 export default function BlogPosts({ blogPosts }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // filtering blog posts by search term
+  let filteredBlogPosts = blogPosts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toString().toLowerCase())
+    );
+  });
+
   return (
-    <div className="blog-posts">
-      {blogPosts.map((blogPost) => (
-        <BlogPost blogPost={blogPost} key={blogPost._id} />
-      ))}
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder="&#61442;"
+        className="blog-posts__input"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <div className="blog-posts">
+        {filteredBlogPosts.map((blogPost) => (
+          <BlogPost blogPost={blogPost} key={blogPost._id} />
+        ))}
+      </div>
+    </>
   );
 }
